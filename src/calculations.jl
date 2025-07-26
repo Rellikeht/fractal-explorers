8f7272f6d7de3a95c16bc8fb62b0b9b16371d33f
Rellikeht <michaelhemperek@gmail.com>
2025-07-26 10:32:49 +0200

    add some nice almost mandelbrot fractals

--- src/calculations.jl
+++ src/calculations.jl
@@ -1,6 +1,12 @@
+#= setup {{{=#
+
 export mandelbrot_calculation,
     drunkenbrot_calculation,
-    heart_mandelbrot_calculation
+    test_mandelbrot_calculation
+
+#= }}}=#
+
+#= standard functions {{{=#
 
 function mandelbrot_calculation(
     start_point::Complex{R},
@@ -15,6 +21,14 @@ function mandelbrot_calculation(
     end
     return maxiter
 end
+
+# TODO julia set
+
+const DEFAULT_CALCULATION = mandelbrot_calculation
+
+#= }}}=#
+
+#= beautiful bugs {{{=#
 
 function drunkenbrot_calculation(
     start_point::Complex{R},
@@ -33,7 +47,9 @@ function drunkenbrot_calculation(
     return maxiter
 end
 
-function triple_mandelbrot_calculation(
+# TODO name them
+
+function test1_mandelbrot_calculation(
     start_point::Complex{R},
     maxiter::I
 )::I where {R<:Real,I<:Integer}
@@ -42,12 +58,66 @@ function triple_mandelbrot_calculation(
         if point.re * point.re + point.im * point.im >= R(4)
             return i
         end
-        point = point * point + start_point
-        point = point * point
+        re, im = point.re, point.im
+        point = Complex{R}(
+            re * re - im * im + re,
+            2 * re * im,
+        )
     end
     return maxiter
 end
 
-# TODO julia set
+function test2_mandelbrot_calculation(
+    start_point::Complex{R},
+    maxiter::I
+)::I where {R<:Real,I<:Integer}
+    point = start_point
+    for i in I(0):maxiter-I(1)
+        if point.re * point.re + point.im * point.im >= R(4)
+            return i
+        end
+        re, im = point.re, point.im
+        point = Complex{R}(
+            re * re - im * im + im,
+            2 * re * im + im,
+        )
+    end
+    return maxiter
+end
 
-const DEFAULT_CALCULATION = mandelbrot_calculation
+function test3_mandelbrot_calculation(
+    start_point::Complex{R},
+    maxiter::I
+)::I where {R<:Real,I<:Integer}
+    point = start_point
+    for i in I(0):maxiter-I(1)
+        if point.re * point.re + point.im * point.im >= R(8)
+            return i
+        end
+        re, im = point.re, point.im
+        point = Complex{R}(
+            re * re - im * im + im,
+            re * im + im,
+        )
+    end
+    return maxiter
+end
+
+function test4_mandelbrot_calculation(
+    start_point::Complex{R},
+    maxiter::I
+)::I where {R<:Real,I<:Integer}
+    point = start_point
+    for i in I(0):maxiter-I(1)
+        if point.re * point.re + point.im * point.im >= R(8)
+            return i
+        end
+        re, im = point.re, point.im
+        re = re * re + im * im + start_point.re
+        im = 2 * im * re + start_point.im
+        point = Complex{R}(re, im)
+    end
+    return maxiter
+end
+
+#= }}}=#
